c517d66a23636bdede9398b0f031ee9f62293708
Valentin Leonov <Valentin.Leonov@noveogroup.com>
2014-01-10 16:46:25 +0700

    (+) Kiwi

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '7.0'
+target :HelloKiwiTests, :exclusive => true do
+   pod 'Kiwi/XCTest'
+end
---
+++ Pods/Pods-HelloKiwiTests-Kiwi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloKiwiTests_Kiwi : NSObject
+@end
+@implementation PodsDummy_Pods_HelloKiwiTests_Kiwi
+@end
